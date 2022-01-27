eada784197e56494b9494f71fd021e664ab7a905
Lucas Eduardo <lucas01.dev@gmail.com>
2022-01-27 17:17:38 -0300

    Listing store products

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('stores', \App\Http\Controllers\StoreController::class);
+Route::apiResource('stores.products', \App\Http\Controllers\ProductController::class)->only(['index']);
